0d55d2c07e12c20437a72b1c304ad80bfca98b19
wuxw7 <wuxw7@asiainfo.com>
2018-09-18 14:52:28 +0800

    商品目录离线存储

--- app/pages/shop/ShopCatalogPage.js
+++ app/pages/shop/ShopCatalogPage.js
@@ -23,6 +23,7 @@ export default class ShopCatalogPage extends Component{
       constructor(props) {
         super(props);
         this.currentPageName = "目录管理";
+        shopMobx.reloadShopCatalogData();
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         // 初始状态
         this.state = {
